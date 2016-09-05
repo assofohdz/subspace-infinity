879ac6d7002d49a8c84890e6e7098accb154b6a7
Paul Speed <pspeed42@users.noreply.github.com>
2016-09-05 14:30:56 -0400

    Fixed the formatting slightly and added the !hasMappings() checks to avoid blowing away existing mappings if they were configured before this.

--- sim-eth-basic/src/main/java/example/view/PlayerMovementFunctions.java
+++ sim-eth-basic/src/main/java/example/view/PlayerMovementFunctions.java
@@ -76,59 +76,77 @@ public class PlayerMovementFunctions {
     
     public static void initializeDefaultMappings( InputMapper inputMapper ) {
     
-        // The joystick Y axes are backwards on game pads... forward
-        // is negative.  So we'll flip it over in the mapping.
-        inputMapper.map( F_THRUST, InputState.Negative, Axis.JOYSTICK_LEFT_Y );
+        if( !inputMapper.hasMappings(F_THRUST) ) {
+            // The joystick Y axes are backwards on game pads... forward
+            // is negative.  So we'll flip it over in the mapping.
+            inputMapper.map(F_THRUST, InputState.Negative, Axis.JOYSTICK_LEFT_Y);
         
-        // Here a similar approach is used to map the W and S keys with
-        // 'S' being negative.  In this way, W and S now act like a joystick
-        // axis.
-        inputMapper.map( F_THRUST, KeyInput.KEY_W );
-        inputMapper.map( F_THRUST, InputState.Negative, KeyInput.KEY_S );
+            // Here a similar approach is used to map the W and S keys with
+            // 'S' being negative.  In this way, W and S now act like a joystick
+            // axis.
+            inputMapper.map(F_THRUST, KeyInput.KEY_W);
+            inputMapper.map(F_THRUST, InputState.Negative, KeyInput.KEY_S);
+        }
         
-        // Strafing is setup similar to move.
-        inputMapper.map( F_STRAFE, Axis.JOYSTICK_LEFT_X );
-        inputMapper.map( F_STRAFE, KeyInput.KEY_D );
-        inputMapper.map( F_STRAFE, InputState.Negative, KeyInput.KEY_A );
+        if( !inputMapper.hasMappings(F_STRAFE) ) {
+            // Strafing is setup similar to move.
+            inputMapper.map(F_STRAFE, Axis.JOYSTICK_LEFT_X);
+            inputMapper.map(F_STRAFE, KeyInput.KEY_D);
+            inputMapper.map(F_STRAFE, InputState.Negative, KeyInput.KEY_A);
+        }
 
-        // Elevation only has key mappings but we still treat it like
-        // one "axis".
-        inputMapper.map( F_ELEVATE, KeyInput.KEY_Q );
-        inputMapper.map( F_ELEVATE, InputState.Negative, KeyInput.KEY_Z ); 
+        if( !inputMapper.hasMappings(F_ELEVATE) ) {
+            // Elevation only has key mappings but we still treat it like
+            // one "axis".
+            inputMapper.map(F_ELEVATE, KeyInput.KEY_Q);
+            inputMapper.map(F_ELEVATE, InputState.Negative, KeyInput.KEY_Z);
+        } 
 
-        // For the mouse and joystick mappings, we remember the mapping object
-        // in case we want to flip it or adjust its sensitivity later.  This
-        // is better than trying to do it in the function handler because it's
-        // quite often input specific.  The user may want to flip the joystick
-        // but not the mouse or adjust sensitivity on one and not the other.
-        MOUSE_X_ROTATE = inputMapper.map( F_X_ROTATE, Axis.MOUSE_X );
-        JOY_X_ROTATE = inputMapper.map( F_X_ROTATE, Axis.JOYSTICK_RIGHT_X );
-        inputMapper.map( F_X_ROTATE, KeyInput.KEY_RIGHT );
-        inputMapper.map( F_X_ROTATE, InputState.Negative, KeyInput.KEY_LEFT );
+        if( !inputMapper.hasMappings(F_X_ROTATE) ) {
+            // For the mouse and joystick mappings, we remember the mapping object
+            // in case we want to flip it or adjust its sensitivity later.  This
+            // is better than trying to do it in the function handler because it's
+            // quite often input specific.  The user may want to flip the joystick
+            // but not the mouse or adjust sensitivity on one and not the other.
+            MOUSE_X_ROTATE = inputMapper.map(F_X_ROTATE, Axis.MOUSE_X);
+            JOY_X_ROTATE = inputMapper.map(F_X_ROTATE, Axis.JOYSTICK_RIGHT_X);
+            inputMapper.map(F_X_ROTATE, KeyInput.KEY_RIGHT);
+            inputMapper.map(F_X_ROTATE, InputState.Negative, KeyInput.KEY_LEFT);
+        } else {
+            // Need to lookup the MOUSE_X_ROTATE and JOY_X_ROTATE if we
+            // need them at some point
+        }            
 
-        MOUSE_Y_ROTATE = inputMapper.map( F_Y_ROTATE, Axis.MOUSE_Y );
-        JOY_Y_ROTATE = inputMapper.map( F_Y_ROTATE, Axis.JOYSTICK_RIGHT_Y );
-        inputMapper.map( F_Y_ROTATE, KeyInput.KEY_UP );
-        inputMapper.map( F_Y_ROTATE, InputState.Negative, KeyInput.KEY_DOWN );
+        if( !inputMapper.hasMappings(F_Y_ROTATE) ) {
+            MOUSE_Y_ROTATE = inputMapper.map(F_Y_ROTATE, Axis.MOUSE_Y);
+            JOY_Y_ROTATE = inputMapper.map(F_Y_ROTATE, Axis.JOYSTICK_RIGHT_Y);
+            inputMapper.map(F_Y_ROTATE, KeyInput.KEY_UP);
+            inputMapper.map(F_Y_ROTATE, InputState.Negative, KeyInput.KEY_DOWN);
+        } else {
+            // Need to lookup the MOUSE_Y_ROTATE and JOY_X_ROTATE if we
+            // need them at some point
+        }            
 
-        // Here we give run its own function.  Note that it was also possible
-        // to treat running like an axis used instead of MOVE by combining
-        // keys.  For example:  
-        //   map( F_BOOST, KeyInput.KEY_LSHIFT, KeyInput.KEY_W )
-        // Another approach would have been to set the 'scale' of the non-run
-        // mapping to 0.5 or something and simply map SHIFT+W without the
-        // scaling.  That would have done two things:
-        // 1) the camera code simply needs to look for MOVE and not worry
-        //      about a separate 'run mode' because walking will be half-speed
-        //      move anyway.
-        // 2) the joystick would always be running which means the player simply
-        //      runs by using full stick.
-        // 
-        // Instead, I've opted for a run mode toggle.  Mostly because it demonstrates
-        // state mapping where as all of the other inputs so far are going to
-        // be treated as analog.  That means our joystick needs a run button also.       
-        inputMapper.map( F_BOOST, KeyInput.KEY_LSHIFT );
-        inputMapper.map( F_BOOST, Button.JOYSTICK_RIGHT1 );
+        if( !inputMapper.hasMappings(F_BOOST) ) {
+            // Here we give run its own function.  Note that it was also possible
+            // to treat running like an axis used instead of MOVE by combining
+            // keys.  For example:  
+            //   map(F_BOOST, KeyInput.KEY_LSHIFT, KeyInput.KEY_W )
+            // Another approach would have been to set the 'scale' of the non-run
+            // mapping to 0.5 or something and simply map SHIFT+W without the
+            // scaling.  That would have done two things:
+            // 1) the camera code simply needs to look for MOVE and not worry
+            //      about a separate 'run mode' because walking will be half-speed
+            //      move anyway.
+            // 2) the joystick would always be running which means the player simply
+            //      runs by using full stick.
+            // 
+            // Instead, I've opted for a run mode toggle.  Mostly because it demonstrates
+            // state mapping where as all of the other inputs so far are going to
+            // be treated as analog.  That means our joystick needs a run button also.       
+            inputMapper.map(F_BOOST, KeyInput.KEY_LSHIFT);
+            inputMapper.map(F_BOOST, Button.JOYSTICK_RIGHT1);
+        }
     }
     
 }    
