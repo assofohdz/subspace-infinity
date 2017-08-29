a2d4590555baf4e65552181415d6dc7b1f2219d4
HenSSon <beluranos@gmail.com>
2017-08-29 19:36:10 +0200

    - add type-specifications

--- sim-eth-es/src/main/java/example/es/TowerTypes.java
+++ sim-eth-es/src/main/java/example/es/TowerTypes.java
@@ -1,13 +1,26 @@
 package example.es;
 
+import com.simsilica.es.Entity;
 import com.simsilica.es.EntityData;
+import com.simsilica.es.EntityId;
 
 public class TowerTypes {
 
-    
+    private static int MSEC = 1000*1000;
     public static final String TOWER1 = "tower1";
 
-    public static TowerType tower1(EntityData ed) {
+    public static TowerType tower1(EntityData ed, EntityId id) {
+        // tower type specifications
+        ed.setComponents(id, 
+                new Range(10), 
+                ProjectileTypes.bullet(ed),
+                AttackMethodTypes.random(ed),
+                new AttackVelocity(500),
+                new AttackRate(1000*MSEC),
+                new RotationSpeed(3.14));
+   
+        
+        
         return TowerType.create(TOWER1, ed);
     }
 }
