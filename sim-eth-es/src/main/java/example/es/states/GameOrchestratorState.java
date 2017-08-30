6d4dd27584aab08c5c30df169fb3b6dfc0b9810c
Asser <Asser@Galadriel>
2017-08-30 12:35:56 +0200

    Added base functionality of game orchestrator

--- sim-eth-es/src/main/java/example/es/states/GameOrchestratorState.java
+++ sim-eth-es/src/main/java/example/es/states/GameOrchestratorState.java
@@ -1,7 +1,11 @@
 package example.es.states;
 
+import com.simsilica.es.EntityData;
+import com.simsilica.es.EntitySet;
 import com.simsilica.sim.AbstractGameSystem;
 import com.simsilica.sim.SimTime;
+import example.es.MobType;
+import example.sim.SimplePhysics;
 
 /**
  * This is a state that holds the rules of the game. This is the state that will
@@ -12,9 +16,28 @@ import com.simsilica.sim.SimTime;
  */
 public class GameOrchestratorState extends AbstractGameSystem {
 
+    //As a baseline, spawn 10 mobs per wave
+    private final int baseline_mobCountPerWave = 10;
+    //As a baseline, add a factor of mobs each wave
+    private final int baseline_waveMobFactor = 2;
+
+    private final int baseline_endWave = 10; //Game length in waves
+    private int currentWave = 0;
+
+    private final int timetoplay = 300; //Game length in seconds
+
+    private SimplePhysics simplePhysics;
+    private EntityData ed;
+    private EntitySet mobs;
+
     @Override
     protected void initialize() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.ed = getSystem(EntityData.class);
+
+        this.simplePhysics = getSystem(SimplePhysics.class);
+
+        //Keep track of how many mobs are in the game currently
+        this.mobs = ed.getEntities(MobType.class);
     }
 
     @Override
@@ -24,7 +47,18 @@ public class GameOrchestratorState extends AbstractGameSystem {
 
     @Override
     public void update(SimTime tpf) {
+        //Here we should account for the game type
 
+        if (mobs.applyChanges()) {
+            //If there are no mobs, start spawning next wave
+            if (mobs.size() == 0) {
+
+                currentWave++;
+                if (currentWave < baseline_endWave) {
+                    spawnWave(currentWave);
+                }
+            }
+        }
     }
 
     @Override
@@ -33,5 +67,13 @@ public class GameOrchestratorState extends AbstractGameSystem {
 
     @Override
     public void stop() {
+    }
+
+    //Spawn the required waves for the game
+    private void spawnWave(int wave) {
+        //TODO: This could be done on a seperate thread so as not to hold the game thread
+        for (int i = 0; i < baseline_mobCountPerWave * currentWave * baseline_waveMobFactor; i++) {
+            //TODO: Create mob, set it steering towards the base
+        }
     }
 }
