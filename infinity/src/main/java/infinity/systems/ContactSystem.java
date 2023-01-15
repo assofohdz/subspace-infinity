1c13361d066d227d970a6617aa51d306ab4bb14a
Asser Fahrenholz <asser@fahrenholz.dk>
2023-01-15 19:16:44 +0100

    Had an old ContactSystem lying around, this is meant to act as both dispatcher and first filter (like a ship not being "hit" by its own bullets/bombs etc.)

--- infinity/src/main/java/infinity/systems/ContactSystem.java
+++ infinity/src/main/java/infinity/systems/ContactSystem.java
@@ -5,129 +5,134 @@
  */
 package infinity.systems;
 
-import java.util.logging.Logger;
-
+import com.google.common.reflect.TypeToken;
 import com.simsilica.es.EntityData;
 import com.simsilica.es.EntityId;
 import com.simsilica.es.EntitySet;
-import com.simsilica.ext.mphys.BinEntityManager;
 import com.simsilica.ext.mphys.MPhysSystem;
 import com.simsilica.mblock.phys.MBlockShape;
 import com.simsilica.mphys.*;
 import com.simsilica.sim.AbstractGameSystem;
 import com.simsilica.sim.SimTime;
-
 import infinity.es.CollisionCategory;
 import infinity.es.Parent;
 import infinity.sim.CategoryFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- *
  * @author AFahrenholz
  */
-public class ContactSystem extends AbstractGameSystem implements ContactListener<EntityId, MBlockShape> {
+public class ContactSystem<K, S extends AbstractShape> extends AbstractGameSystem
+    implements ContactListener<EntityId, MBlockShape> {
 
-    private static final Logger log = Logger.getLogger(ContactSystem.class.getName());
+  static Logger log = LoggerFactory.getLogger(ContactSystem.class);
+  private final DynArray<ContactListener<K, S>> listeners =
+      new DynArray<>(new TypeToken<ContactListener<K, S>>() {});
+  EntitySet categoryFilters;
+  private EntityData ed;
+  private MPhysSystem<?> physics;
+  private PhysicsSpace<EntityId, ?> space;
 
-    private EntityData ed;
-    private MPhysSystem<?> physics;
-    private PhysicsSpace<EntityId, ?> space;
-    private BinIndex binIndex;
-    private BinEntityManager binEntityManager;
-    EntitySet categoryFilters;
+  @Override
+  public void newContact(Contact contact) {
+    final RigidBody<EntityId, MBlockShape> bodyOne = contact.body1;
+    final AbstractBody<EntityId, MBlockShape> bodyTwo = contact.body2;
 
-    @Override
-    public void newContact(final Contact<EntityId, MBlockShape> contact) {
-        final RigidBody<EntityId, MBlockShape> bodyOne = contact.body1;
-        final AbstractBody<EntityId, MBlockShape> bodyTwo = contact.body2;
+    //Body1 is always a rigidbody
+    //If body two is not null, we are dealing with a collision between a rigidbody (body1) and a rigidbody or a staticbody (body2)
+    if (bodyTwo != null) {
+      final EntityId one = bodyOne.id;
+      final EntityId two = bodyTwo.id;
 
-        if (bodyOne != null && bodyTwo != null) {
-            final EntityId one = contact.body1.id;
-            final EntityId two = contact.body2.id;
+      if (categoryFilters.containsId(two) && categoryFilters.containsId(one)) {
+        final CategoryFilter filterOne =
+            categoryFilters.getEntity(one).get(CollisionCategory.class).getFilter();
+        final CategoryFilter filterTwo =
+            categoryFilters.getEntity(two).get(CollisionCategory.class).getFilter();
 
-            if (categoryFilters.containsId(two) && categoryFilters.containsId(one)) {
-                final CategoryFilter filterOne = categoryFilters.getEntity(one).get(CollisionCategory.class)
-                        .getFilter();
-                final CategoryFilter filterTwo = categoryFilters.getEntity(two).get(CollisionCategory.class)
-                        .getFilter();
-
-                if (!filterTwo.isAllowed(filterOne)) {
-                    log.info("Disabling contact: "+contact.toString());
-                    contact.disable();
-                    return;
-                }
-            }
-
-            final Parent parentOfOne = ed.getComponent(one, Parent.class);
-
-            if (parentOfOne != null && parentOfOne.getParentEntity().compareTo(two) == 0) {
-                // We have a parent on entity one and its equal to two
-                log.info("Disabling contact: "+contact.toString());
-                contact.disable();
-                return;
-            }
-
-            final Parent parentOfTwo = ed.getComponent(two, Parent.class);
-            if (parentOfTwo != null && parentOfTwo.getParentEntity().compareTo(one) == 0) {
-                // We have a parent on entity two and its equal to one
-                log.info("Disabling contact: "+contact.toString());
-                contact.disable();
-                return;
-            }
-
-            log.info("Collision between: " + bodyOne + " and " + bodyTwo);
-
-        } else {
-            // This happens when a dynamic collides with a static body or the world
-            if (bodyOne != null) {
-                log.info("Collided: " + bodyOne.toString() + " with null");
-            } else if (bodyTwo != null) {
-                log.info("Collided: " + bodyTwo.toString() + " with null");
-            }
-
-            contact.restitution = 1;
+        if (!filterTwo.isAllowed(filterOne)) {
+          log.debug("Disabling contact because of collision categories: " + contact);
+          contact.disable();
+          return;
         }
+      }
 
+      final Parent parentOfOne = ed.getComponent(one, Parent.class);
+
+      if (parentOfOne != null && parentOfOne.getParentEntityId().compareTo(two) == 0) {
+        // We have a parent on entity one and its equal to two
+        log.debug("Disabling contact because one is a parent of the other: " + contact);
+        contact.disable();
+        return;
+      }
+
+      final Parent parentOfTwo = ed.getComponent(two, Parent.class);
+      if (parentOfTwo != null && parentOfTwo.getParentEntityId().compareTo(one) == 0) {
+        // We have a parent on entity two and its equal to one
+        log.debug("Disabling contact because one is a parent of the other: " + contact);
+        contact.disable();
+        return;
+      }
+
+      log.debug("Collision between: " + bodyOne + " and " + bodyTwo);
+
+    } else{
+        log.debug("Collided: " + bodyTwo + " with null");
+        //Restitution should make sure the bounce conserves the energy completely
+        contact.restitution = 1;
+      }
+
+    //Now that we have filtered the basics, lets send it to the various systems listening for contacts
+    for (ContactListener l : listeners) {
+      l.newContact(contact);
+    }
+  }
+
+  @Override
+  public void stop() {
+    super.stop(); // To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public void update(final SimTime time) {
+    super.update(time); // To change body of generated methods, choose Tools | Templates.
+
+    categoryFilters.applyChanges();
+  }
+
+  @Override
+  public void start() {
+    super.start(); // To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  protected void initialize() {
+    ed = getSystem(EntityData.class);
+    if (ed == null) {
+      throw new RuntimeException(getClass().getName() + " system requires an EntityData object.");
+    }
+    physics = getSystem(MPhysSystem.class);
+    if (physics == null) {
+      throw new RuntimeException(getClass().getName() + " system requires the MPhysSystem system.");
     }
 
-    @Override
-    public void stop() {
-        super.stop(); // To change body of generated methods, choose Tools | Templates.
-    }
+    space = physics.getPhysicsSpace();
 
-    @Override
-    public void update(final SimTime time) {
-        super.update(time); // To change body of generated methods, choose Tools | Templates.
+    categoryFilters = ed.getEntities(CollisionCategory.class);
+  }
 
-        categoryFilters.applyChanges();
-    }
+  @Override
+  protected void terminate() {
+    categoryFilters.release();
+    categoryFilters = null;
+  }
 
-    @Override
-    public void start() {
-        super.start(); // To change body of generated methods, choose Tools | Templates.
-    }
+  public void addListener(ContactListener listener) {
+    this.listeners.add(listener);
+  }
 
-    @Override
-    protected void initialize() {
-        ed = getSystem(EntityData.class);
-        if (ed == null) {
-            throw new RuntimeException(getClass().getName() + " system requires an EntityData object.");
-        }
-        physics = getSystem(MPhysSystem.class);
-        if (physics == null) {
-            throw new RuntimeException(getClass().getName() + " system requires the MPhysSystem system.");
-        }
-
-        space = physics.getPhysicsSpace();
-        binIndex = space.getBinIndex();
-        binEntityManager = physics.getBinEntityManager();
-
-        categoryFilters = ed.getEntities(CollisionCategory.class);
-    }
-
-    @Override
-    protected void terminate() {
-        categoryFilters.release();
-        categoryFilters = null;
-    }
+  public void removeListener(ContactListener listener) {
+    this.listeners.remove(listener);
+  }
 }
