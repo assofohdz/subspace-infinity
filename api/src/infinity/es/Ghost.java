92d7619b3f23f6088268dfd22dfccd07dc928bd4
Asser Fahrenholz <asser@fahrenholz.dk>
2023-01-29 17:40:34 +0100

    Added a Ghost EntityComponent

---
+++ api/src/infinity/es/Ghost.java
@@ -0,0 +1,10 @@
+package infinity.es;
+
+import com.simsilica.es.EntityComponent;
+
+public class Ghost implements EntityComponent {
+
+      public Ghost() {
+            //For serialization
+      }
+}
