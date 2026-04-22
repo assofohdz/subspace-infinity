027f2b9c5ba5a0f9706a232c3220fbfd7369d624
assofohdz <asser@fahrenholz.dk>
2026-04-22 16:32:32 +0200

    Fix license headers

--- infinity/src/test/java/infinity/systems/MapSwapReproducerTest.java
+++ infinity/src/test/java/infinity/systems/MapSwapReproducerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026, Asser Fahrenholz
+ * Copyright (c) 2018-2026, Asser Fahrenholz
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
